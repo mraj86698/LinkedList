86f0d374a52627f10f5d4170cb1e7622bdf6c426
MohanRaj <mraj86698@gmail.com>
2022-07-12 19:58:01 +0530

    Added to create Simple Linked List

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -2,9 +2,39 @@ package LinkedList;
 
 public class LinkedList {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/**
+	 * Method to add the elements in linked list
+	 */
 
+	public Node add(Node head, int data) {
+		Node newNode = new Node(data);
+		if (head == null)
+			head = newNode;
+		else if (head != null) {
+			Node temp = head;
+				while (temp.next != null) {
+					temp = temp.next;
+				}
+			temp.next = newNode;
+		}
+		return head;
 	}
+
+	/**
+	 * Method to diplay the elements in the linked list.
+	 */
+
+	 public void display(Node head) {
+	        Node temp = head;
+	        if(temp == null) {
+	            System.out.println("LinkedList is empty");
+	        }else {
+	            while(temp.next!=null) {
+	                System.out.print(temp.data+" ");
+	                temp = temp.next;
+	            }
+	            System.out.println(temp.data);
+	        }
+	   }
 
 }
--- src/LinkedList/LinkedListMain.java
+++ src/LinkedList/LinkedListMain.java
@@ -2,7 +2,13 @@ package LinkedList;
 
 public class LinkedListMain {
 	public static void main(String args[]) {
-		System.out.println("It is an LinkedList");
+		System.out.println("Welcome to the Linkedlist Program");
+		LinkedList list = new LinkedList();
+
+        Node head = list.add(null, 56);
+        head = list.add(head, 30);
+        head = list.add(head, 70);
+        list.display(head);
 	}
 
 }
