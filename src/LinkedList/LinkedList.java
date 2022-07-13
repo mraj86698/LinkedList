c77058bac48ea01d6feeb462e55b0a7e86369b93
MohanRaj <mraj86698@gmail.com>
2022-07-13 13:59:21 +0530

    Added Simple Linked List

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -1,40 +1,92 @@
 package LinkedList;
 
-public class LinkedList {
 
-	/**
-	 * Method to add the elements in linked list
-	 */
+	class Node
+	{
+		private Node next;
+		private int data;
 
-	public Node add(Node head, int data) {
-		Node newNode = new Node(data);
-		if (head == null)
-			head = newNode;
-		else if (head != null) {
-			Node temp = head;
-				while (temp.next != null) {
-					temp = temp.next;
-				}
-			temp.next = newNode;
+		public Node()
+		{
+			data= 0;
+			next = null;
 		}
-		return head;
+		public Node(int d, Node node)
+		{
+			data = d;
+			next = node;
+		}
+		public void setNext(Node n)
+		{
+			this.next = n;
+		}
+
+		public Node getNext()
+		{
+			return next;
+		}
+
+		public void setData(int da)
+		{
+			this.data = da;
+		}
+
+		public int getData() {
+			return data;
+		}
+
 	}
 
-	/**
-	 * Method to diplay the elements in the linked list.
-	 */
+	public class LinkedList {
 
-	 public void display(Node head) {
-	        Node temp = head;
-	        if(temp == null) {
-	            System.out.println("LinkedList is empty");
-	        }else {
-	            while(temp.next!=null) {
-	                System.out.print(temp.data+" ");
-	                temp = temp.next;
-	            }
-	            System.out.println(temp.data);
-	        }
-	   }
+		private int size;
+		private Node start;
+
+		public LinkedList()
+		{
+			size = 0;
+			start = null;
+		}
+
+		public boolean isEmpty()
+		{
+			if(start == null)
+				return (true);
+			else
+				return (false);
+		}
+
+		public int getListSize()
+		{
+
+			return size;
+		}
+
+		public void display()
+		{
+			Node temp;
+			if(isEmpty())
+				System.out.println("List is Empty");
+			else
+			{
+				temp = start;
+				for (int i = 1; i <= size; i++)
+				{
+					System.out.print(temp.getData()+" ");
+					temp = temp.getNext();
+
+				}
+				System.out.println();
+			}
+		}
+
+	public void insertAtFirst(int value)
+	{
+		Node n = new Node();
+		n.setData(value);
+		n.setNext(start);
+		start = n;
+		size++;
+	}
 
 }
--- src/LinkedList/LinkedListMain.java
+++ src/LinkedList/LinkedListMain.java
@@ -4,11 +4,36 @@ public class LinkedListMain {
 	public static void main(String args[]) {
 		System.out.println("Welcome to the Linkedlist Program");
 		LinkedList list = new LinkedList();
+		boolean flag = true;
+		while(flag)
+		{
+			System.out.println("1. Insert at first position\n2. Display List ");
+			int choice = InputUtility.inputInteger();
 
-        Node head = list.add(null, 56);
-        head = list.add(head, 30);
-        head = list.add(head, 70);
-        list.display(head);
+			switch (choice)
+			{
+			case 1:
+				System.out.println("How many items do u want to insert into the list: ");
+				int totalItem = InputUtility.inputInteger();
+				System.out.println("Enter Item");
+				for(int i = 0; i < totalItem; i++) {
+					int item = InputUtility.inputInteger();
+					list.insertAtFirst(item);
+				}
+				System.out.println("List Item Added Successfully");
+
+				break;
+			case 2:
+				System.out.println("List Items : ");
+				list.display();
+				break;
+			default:
+				System.out.println("Invalid Position");
+				break;
+			}
+		}
+
+
 	}
 
 }
